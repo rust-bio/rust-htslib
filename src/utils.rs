d04183fe0bb835094aac8d057a6b03a2c5e70a1e
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-21 10:28:07 -0400

    Added missing file.

---
+++ src/utils.rs
@@ -0,0 +1,26 @@
+
+use std::ptr;
+use std::ffi;
+use std::path::Path;
+
+/// Copies data from `src` to `dst`
+/// TODO remove once stable in standard library.
+///
+/// Panics if the length of `dst` is less than the length of `src`.
+#[inline]
+pub fn copy_memory(src: &[u8], dst: &mut [u8]) {
+    let len_src = src.len();
+    assert!(dst.len() >= len_src);
+    // `dst` is unaliasable, so we know statically it doesn't overlap
+    // with `src`.
+    unsafe {
+        ptr::copy_nonoverlapping(src.as_ptr(),
+                                 dst.as_mut_ptr(),
+                                 len_src);
+    }
+}
+
+
+pub fn path_to_cstring<P: AsRef<Path>>(path: &P) -> Option<ffi::CString> {
+    path.as_ref().to_str().and_then(|p| ffi::CString::new(p).ok())
+}
