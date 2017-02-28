39d4610cf7955cefd7cdb56a0a33e707b294f84a
sitag <sitag604@gmail.com>
2017-02-27 21:39:24 -0800

    sam writer stdout test added

---
+++ src/bin/test_bam_decompress.rs
@@ -0,0 +1,10 @@
+extern crate rust_htslib;
+use self::rust_htslib::bam::SAMWriter;
+
+use std::io::Write;
+
+pub fn main(){
+  let example_bam = "./test/bam2sam_test.bam";
+  let result = SAMWriter::from_bam_with_filter(example_bam, &"-", |_|{Some(true)});	
+  assert!(writeln!(&mut std::io::stderr(), "{:?}", result).is_ok());
+}
