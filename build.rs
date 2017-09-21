d5ab5aa3d6a2bf914147d615d8f14a12ab12e74f
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-09-21 16:29:01 +0200

    Fix issue #19.

--- build.rs
+++ build.rs
@@ -3,29 +3,42 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
+extern crate fs_utils;
+
+use fs_utils::copy::copy_directory;
 
 use std::process::Command;
 use std::env;
+use std::fs;
 
 fn main() {
-    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
-    if Command::new("make").current_dir("htslib")
+    let out = env::var("OUT_DIR").unwrap();
+    let within_out = |p| format!("{}/{}", out, p);
+    fs::remove_dir_all(within_out("htslib"));
+    copy_directory("htslib", &out).unwrap();
+
+    if Command::new("make").current_dir(within_out("htslib"))
                            .arg("CFLAGS=-g -Wall -O2 -fPIC")
                            .arg("lib-static")
+                           .arg("-B")
                            .status().unwrap()
                            .success() != true {
         panic!("failed to build htslib");
     }
-    if Command::new("gcc").arg("-Ihtslib")
+    if Command::new("gcc").arg("-I").arg(within_out("htslib"))
                           .arg("-c")
                           .arg("src/htslib/macros.c")
+                          .arg("-o").arg(within_out("macros.o"))
                           .status().unwrap().success() != true {
         panic!("error building macro wrapper");
     }
-    if Command::new("ar").arg("r").arg("libmacros.a").arg("macros.o").status().unwrap().success() != true {
+    if Command::new("ar").arg("r")
+                         .arg(within_out("libmacros.a"))
+                         .arg(within_out("macros.o"))
+                         .status().unwrap().success() != true {
         panic!("error buiding macro wrapper");
     }
 
-    println!("cargo:rustc-flags=-L {root}/htslib -L {root} -l static=hts -l static=macros -l z",
-             root=root);
+    println!("cargo:rustc-flags=-L {out}/htslib -L {out} -l static=hts -l static=macros -l z",
+             out=out);
 }
