5ec4f570186b4e04781962b18ac64a05d3cc44a5
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-11-29 15:26:41 +0100

    Improved buffer API.

--- src/bcf/buffer.rs
+++ src/bcf/buffer.rs
@@ -3,7 +3,7 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use std::collections::VecDeque;
+use std::collections::{vec_deque, VecDeque};
 use std::mem;
 use std::error::Error;
 
@@ -13,13 +13,20 @@ use bcf;
 /// A buffer for BCF records. This allows access regions in a sorted BCF file while iterating
 /// over it in a single pass.
 /// The buffer is implemented as a ringbuffer, such that extension or movement to the right has
-/// linear complexity.
+/// linear complexity. The buffer does not use any indexed random access. Hence, for getting a
+/// region at the very end of the BCF, you will have to wait until all records before have
+/// been read.
 pub struct RecordBuffer {
     reader: bcf::Reader,
     ringbuffer: VecDeque<bcf::Record>,
     ringbuffer2: VecDeque<bcf::Record>,
-    last_end: Option<u32>
+    overflow: Option<bcf::Record>
 }
+
+
+unsafe impl Sync for RecordBuffer {}
+unsafe impl Send for RecordBuffer {}
+
 
 impl RecordBuffer {
     /// Create new buffer.
@@ -28,7 +35,7 @@ impl RecordBuffer {
             reader: reader,
             ringbuffer: VecDeque::new(),
             ringbuffer2: VecDeque::new(),
-            last_end: None
+            overflow: None
         }
     }
 
@@ -60,9 +67,15 @@ impl RecordBuffer {
     /// Fill the buffer with variants in the given window. The start coordinate has to be left of
     /// the start coordinate of any previous `fill` operation.
     /// Coordinates are 0-based, and end is exclusive.
-    pub fn fill(&mut self, chrom: &[u8], start: u32, end: u32) -> Result<(), Box<Error>> {
+    pub fn fetch(&mut self, chrom: &[u8], start: u32, end: u32) -> Result<(), Box<Error>> {
         let rid = try!(self.reader.header.name2rid(chrom));
 
+        // move overflow from last fill into ringbuffer
+        if self.overflow.is_some() {
+            self.ringbuffer.push_back(self.overflow.take().unwrap());
+        }
+
+        // shrink and swap
         match (self.last_rid(), self.next_rid()) {
             (Some(last_rid), _) => {
                 if last_rid != rid {
@@ -98,7 +111,7 @@ impl RecordBuffer {
                 if rec_rid == rid {
                     if pos >= end {
                         // Record is beyond our window. Store it anyways but stop.
-                        self.ringbuffer.push_back(rec);
+                        self.overflow = Some(rec);
                         break;
                     } else if pos >= start {
                         // Record is within our window.
@@ -121,14 +134,12 @@ impl RecordBuffer {
             }
         }
 
-        self.last_end = Some(end);
-
         Ok(())
     }
 
-    pub fn fetch(&self) -> Vec<&bcf::Record> {
-        // if self.last_end is None, self.ringbuffer is empty
-        self.ringbuffer.iter().filter(|rec| rec.pos() < self.last_end.unwrap()).collect()
+    /// Iterate over records that have been fetched with `fetch`.
+    pub fn iter(&self) -> vec_deque::Iter<bcf::Record> {
+        self.ringbuffer.iter()
     }
 }
 
@@ -138,17 +149,26 @@ mod tests {
     use super::*;
     use bcf;
     use bcf::record::Numeric;
+    use itertools::Itertools;
 
     #[test]
     fn test_buffer() {
         let reader = bcf::Reader::from_path(&"test/test.bcf").unwrap();
         let mut buffer = RecordBuffer::new(reader);
 
-        buffer.fill(b"1", 100, 10023).unwrap();
-        let records = buffer.fetch();
+        buffer.fetch(b"1", 100, 10023).unwrap();
+        {
+            let records = buffer.iter().collect_vec();
+            assert_eq!(records.len(), 2);
+            assert_eq!(records[0].pos(), 10021);
+            assert_eq!(records[1].pos(), 10022);
+        }
 
-        assert_eq!(records.len(), 2);
-        assert_eq!(records[0].pos(), 10021);
-        assert_eq!(records[1].pos(), 10022);
+        buffer.fetch(b"1", 10023, 10024);
+        {
+            let records = buffer.iter().collect_vec();
+            assert_eq!(records.len(), 1);
+            assert_eq!(records[0].pos(), 10023);
+        }
     }
 }
