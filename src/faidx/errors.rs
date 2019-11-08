4478e1d90e36cb532fa0c9f09682da1ef3378295
Manuel Landesfeind <Manuel.Landesfeind@evotec.com>
2019-11-08 16:09:25 +0100

    Add missing file

---
+++ src/faidx/errors.rs
@@ -0,0 +1,14 @@
+use snafu::Snafu;
+use std::path::PathBuf;
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Snafu, Debug, PartialEq)]
+#[snafu(visibility = "pub")]
+pub enum Error {
+    #[snafu(display("file not found: {}", path.display()))]
+    FileNotFound { path: PathBuf },
+    #[snafu(display("invalid (non-unique) characters in path"))]
+    NonUnicodePath,
+}
+ 
