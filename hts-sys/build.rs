c767e0e1a9adb3fd5296dbc7616bbc4fafccc889
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-14 17:13:53 +1000

    Temporarily comment out sed modifications against htslib's makefile, focusing on static compilation

--- hts-sys/build.rs
+++ hts-sys/build.rs
@@ -11,22 +11,22 @@ use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn sed_htslib_makefile(out: &PathBuf, patterns: &[&str], feature: &str) {
-    for pattern in patterns {
-        if !Command::new("sed")
-            .current_dir(out.join("htslib"))
-            .arg("-i")
-            .arg("-e")
-            .arg(pattern)
-            .arg("Makefile")
-            .status()
-            .unwrap()
-            .success()
-        {
-            panic!("failed to strip {} support", feature);
-        }
-    }
-}
+// fn sed_htslib_makefile(out: &PathBuf, patterns: &[&str], feature: &str) {
+//     for pattern in patterns {
+//         if !Command::new("sed")
+//             .current_dir(out.join("htslib"))
+//             .arg("-i")
+//             .arg("-e")
+//             .arg(pattern)
+//             .arg("Makefile")
+//             .status()
+//             .unwrap()
+//             .success()
+//         {
+//             panic!("failed to strip {} support", feature);
+//         }
+//     }
+// }
 
 fn main() {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -39,42 +39,42 @@ fn main() {
         cfg.warnings(true).static_flag(false).pic(true); 
     }
 
-    if let Ok(z_inc) = env::var("DEP_Z_INCLUDE") {
-        if !want_static {
-            cfg.include(z_inc);
-        }
-    }
+    // if let Ok(z_inc) = env::var("DEP_Z_INCLUDE") {
+    //     if !want_static {
+    //         cfg.include(z_inc);
+    //     }
+    // }
 
     if !out.join("htslib").exists() {
         copy_directory("htslib", &out).unwrap();
     }
 
-    let use_bzip2 = env::var("CARGO_FEATURE_BZIP2").is_ok();
-    if !use_bzip2 {
-        let bzip2_patterns = vec!["s/ -lbz2//", "/#define HAVE_LIBBZ2/d"];
-        sed_htslib_makefile(&out, &bzip2_patterns, "bzip2");
-    } else if let Ok(inc) = env::var("DEP_BZIP2_ROOT")
-        .map(PathBuf::from)
-        .map(|path| path.join("include"))
-    {
-        cfg.include(inc);
-    }
+    // let use_bzip2 = env::var("CARGO_FEATURE_BZIP2").is_ok();
+    // if !use_bzip2 {
+    //     let bzip2_patterns = vec!["s/ -lbz2//", "/#define HAVE_LIBBZ2/d"];
+    //     sed_htslib_makefile(&out, &bzip2_patterns, "bzip2");
+    // } else if let Ok(inc) = env::var("DEP_BZIP2_ROOT")
+    //     .map(PathBuf::from)
+    //     .map(|path| path.join("include"))
+    // {
+    //     cfg.include(inc);
+    // }
 
-    let use_lzma = env::var("CARGO_FEATURE_LZMA").is_ok();
-    if !use_lzma && want_static {
-        let lzma_patterns = vec!["s/ -llzma//", "/#define HAVE_LIBLZMA/d"];
-        sed_htslib_makefile(&out, &lzma_patterns, "lzma");
-    } else if let Ok(inc) = env::var("DEP_LZMA_INCLUDE").map(PathBuf::from) {
-        cfg.include(inc);
-    }
+    // let use_lzma = env::var("CARGO_FEATURE_LZMA").is_ok();
+    // if !use_lzma && want_static {
+    //     let lzma_patterns = vec!["s/ -llzma//", "/#define HAVE_LIBLZMA/d"];
+    //     sed_htslib_makefile(&out, &lzma_patterns, "lzma");
+    // } else if let Ok(inc) = env::var("DEP_LZMA_INCLUDE").map(PathBuf::from) {
+    //     cfg.include(inc);
+    // }
 
-    let use_curl = env::var("CARGO_FEATURE_CURL").is_ok();
-    if !use_curl {
-        let curl_patterns = vec!["s/ -lcurl//", "/#define HAVE_LIBCURL/d"];
-        sed_htslib_makefile(&out, &curl_patterns, "curl");
-    } else if let Ok(inc) = env::var("DEP_CURL_INCLUDE").map(PathBuf::from) {
-        cfg.include(inc);
-    }
+    // let use_curl = env::var("CARGO_FEATURE_CURL").is_ok();
+    // if !use_curl {
+    //     let curl_patterns = vec!["s/ -lcurl//", "/#define HAVE_LIBCURL/d"];
+    //     sed_htslib_makefile(&out, &curl_patterns, "curl");
+    // } else if let Ok(inc) = env::var("DEP_CURL_INCLUDE").map(PathBuf::from) {
+    //     cfg.include(inc);
+    // }
 
     let tool = cfg.get_compiler();
     let (cc_path, cflags_env) = (tool.path(), tool.cflags_env());
@@ -83,7 +83,7 @@ fn main() {
     //let cppflags = env::var("CPPFLAGS").unwrap_or_default();
     let _ldflags= env::var("LDFLAGS").unwrap_or_default();
     let host = env::var("HOST").unwrap_or_default();
-    // This ./configure step is necessary to include the htslib plugins in the resulting libhts.a (hfile_s3.o, hfile_s3_writer.o, etc...)
+    // Those two steps are necessary to include the htslib plugins in the resulting libhts.a (hfile_s3.o, hfile_s3_writer.o, etc...)
     if !Command::new("autoreconf").current_dir(out.join("htslib")).status().unwrap().success() &&
        !Command::new("./configure").current_dir(out.join("htslib"))
         .arg(format!("--host={}", &host))
@@ -133,6 +133,9 @@ fn main() {
     println!("cargo:rustc-link-lib=static=hts");
     println!("cargo:rustc-link-lib=static=z");
     println!("cargo:rustc-link-lib=static=lzma");
+    // println!("cargo:rustc-link-lib=static=ssl");
+    // println!("cargo:rustc-link-lib=static=crypto");
+    println!("cargo:rustc-link-lib=static=curl");
     println!("cargo:rerun-if-changed=wrapper.c");
     println!("cargo:rerun-if-changed=wrapper.h");
     for htsfile in glob("htslib/**/*").unwrap() {
