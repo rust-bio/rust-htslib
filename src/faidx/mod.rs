16d3c432dd7f6066679e7cba240a71d79021a25a
Manuel Landesfeind <manuel@landesfeind.de>
2020-08-06 21:29:33 +0200

    Apply renaming of errors to faidx

--- src/faidx/mod.rs
+++ src/faidx/mod.rs
@@ -13,7 +13,7 @@ use url::Url;
 
 use crate::htslib;
 
-use crate::error::{Error, Result};
+use crate::errors::{Error, Result};
 
 fn path_as_bytes<'a, P: 'a + AsRef<Path>>(path: P, must_exist: bool) -> Result<Vec<u8>> {
     if path.as_ref().exists() || !must_exist {
