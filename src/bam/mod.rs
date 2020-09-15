cc17de485fca26aa0caccc1e9315fccc5a75689a
Till Hartmann <till.hartmann@udo.edu>
2020-09-15 17:14:40 +0200

    fix missing slash for doc comment

--- src/bam/mod.rs
+++ src/bam/mod.rs
@@ -133,7 +133,7 @@ pub trait Read: Sized {
     /// use rust_htslib::bam::{Error, Read, Reader, Record};
     /// use rust_htslib::htslib; // for BAM_F*
     /// let mut bam = Reader::from_path(&"test/test.bam").unwrap();
-    //
+    ///
     /// for read in
     ///     bam.rc_records()
     ///     .map(|x| x.expect("Failure parsing Bam file"))
