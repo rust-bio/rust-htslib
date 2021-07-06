dbdced6dbc9c35e91cba5ce79757f33a8e242f02
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-07-06 22:02:47 +0200

    refactor: fmt

--- hts-sys/src/lib.rs
+++ hts-sys/src/lib.rs
@@ -3,9 +3,9 @@
 #![allow(non_snake_case)]
 #![allow(clippy::all)]
 #![allow(improper_ctypes)]
-//! This module exposes the raw [HTSlib](https://github.com/samtools/htslib) bindings for reading and writing 
+//! This module exposes the raw [HTSlib](https://github.com/samtools/htslib) bindings for reading and writing
 //! genomics file formats like SAM, BAM, CRAM, VCF, BCF and tabix.
-//! Instead of using this crate directly, it is recommended to use [rust-htslib](https://docs.rs/rust-htslib) 
+//! Instead of using this crate directly, it is recommended to use [rust-htslib](https://docs.rs/rust-htslib)
 //! instead, which has a more idiomatic and high-level API and builds on top of this crate.
 
 #[cfg(feature = "bzip2")]
