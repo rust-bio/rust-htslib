907dabbe960dffa97af69d899e484e555e24ca1c
Jasper Krauter <jasper.krauter@helsinki.fi>
2025-09-18 15:41:33 +0300

    fix(clippy): Fix clippy lints on elided lifetimes

--- src/bam/record.rs
+++ src/bam/record.rs
@@ -833,7 +833,7 @@ impl Record {
     ///
     /// When an error occurs, the `Err` variant will be returned
     /// and the iterator will not be able to advance anymore.
-    pub fn aux_iter(&self) -> AuxIter<'_> {
+    pub fn aux_iter(&'_ self) -> AuxIter<'_> {
         AuxIter {
             // In order to get to the aux data section of a `bam::Record`
             // we need to skip fields in front of it
@@ -1272,14 +1272,14 @@ impl Record {
     ///    }
     ///    assert_eq!(mod_count, 14);
     /// ```
-    pub fn basemods_iter(&self) -> Result<BaseModificationsIter<'_>> {
+    pub fn basemods_iter(&'_ self) -> Result<BaseModificationsIter<'_>> {
         BaseModificationsIter::new(self)
     }
 
     /// An iterator that returns all of the modifications for each position as a vector.
     /// This is useful for the case where multiple possible modifications can be annotated
     /// at a single position (for example a C could be 5-mC or 5-hmC)
-    pub fn basemods_position_iter(&self) -> Result<BaseModificationsPositionIter<'_>> {
+    pub fn basemods_position_iter(&'_ self) -> Result<BaseModificationsPositionIter<'_>> {
         BaseModificationsPositionIter::new(self)
     }
 
@@ -1658,7 +1658,7 @@ where
     }
 
     /// Returns an iterator over the array.
-    pub fn iter(&self) -> AuxArrayIter<'_, T> {
+    pub fn iter(&'_ self) -> AuxArrayIter<'_, T> {
         AuxArrayIter {
             index: 0,
             array: self,
